5272bcb42ac2d09f64730b25f514ee99f2f6ca93
VOBLAD <119936951+VOBLAD@users.noreply.github.com>
2023-04-14 01:02:16 +0300

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,5 +1,12 @@
-from django.urls import path, include
+from django.urls import path
+from . import views
+
+
 
 urlpatterns = [
-    path('', include('yodo2.urls')),
+    path('', views.index, name='home'),
+    path('download/', views.download, name='download'),
+    path('download/{done}', views.done, name='done'),
+    path('download/{mp3}', views.mp3, name='mp3'),
 ]
+
